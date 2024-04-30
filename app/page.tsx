ed54ccac6c60d8e73851ef8cf6d746896fab0375
jean-feitani <jeanls.folks@gmail.com>
2024-04-30 20:40:05 -0300

    feat: add restaurants items & list

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,10 @@ import Header from './components/header'
 import Search from './components/search'
 import ProductList from './components/products-list'
 import { Button } from './components/ui/button'
-import { ChevronsRightIcon } from 'lucide-react'
+import { ChevronRightIcon, ChevronsRightIcon } from 'lucide-react'
 import { db } from './lib/prisma'
 import PromoBanner from './components/promo-banner'
+import RestaurantList from './components/restaurant-list'
 
 const Home = async () => {
   const products = await db.product.findMany({
@@ -51,6 +52,20 @@ const Home = async () => {
           src="/promo-banner-02.png"
           alt="A partir de R$17,90 em lanches"
         />
+      </div>
+
+      <div className="space-y-4 py-6">
+        <div className="flex items-center justify-between px-5">
+          <h2 className="font-semibold">Restaurantes Recomendados</h2>
+          <Button
+            variant="ghost"
+            className="h-fit p-0 text-primary hover:bg-transparent"
+          >
+            Ver todos
+            <ChevronRightIcon size={16} />
+          </Button>
+        </div>
+        <RestaurantList />
       </div>
     </>
   )
