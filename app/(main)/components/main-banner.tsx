b24ee0d4c24c7c167611891b6ff569d5155f9d34
jean-feitani <jeanls.folks@gmail.com>
2024-05-13 15:15:11 -0300

    style: fix search in main banner

--- app/(main)/components/main-banner.tsx
+++ app/(main)/components/main-banner.tsx
@@ -7,13 +7,13 @@ const MainBanner = () => {
     <div className="h-[500px]">
       <div className="absolute left-[0] z-[-1] h-[500px] w-[100vw] bg-red-600" />
       <div className="relative flex h-full flex-col justify-center">
-        <h1 className="mb-2 text-5xl font-bold text-white">Está com fome?</h1>
+        <h1 className="mb-2.5 text-5xl font-bold text-white">Está com fome?</h1>
         <p className="mb-6 text-lg text-white">
           Com apenas alguns cliques, encontre refeições acessíveis perto de
           você.
         </p>
 
-        <div className="w-[50%] rounded-md bg-white px-4 py-3">
+        <div className="w-[51%] rounded-md bg-white p-5">
           <Search yellow />
         </div>
 
